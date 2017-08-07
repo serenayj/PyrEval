d70c0502d1cf61382ff782fc00d575c9a5a23ae9
serenayj <yug125@psu.edu>
2017-08-07 15:54:01 -0400

    Fix Scoring

--- Scoring/scoring.py
+++ Scoring/scoring.py
@@ -3,13 +3,17 @@ from lib_scoring import getScore, getLayerSizes, filename, processResults, scusB
 import glob
 import sys
 import csv
+import os
 
 """
 ============================ Input==============================
 """
 
-summaries = list(glob.iglob('../peer_summaries/*'))
+summaries = list(glob.iglob('../Preprocess/peer_summaries/*'))
+# See pyrmaid from "Scoring/pyrs/pyramids/" folder
 pyramid = sys.argv[1]
+#for testing
+#pyramid = '../Scoring/pyrs/pyramids/pyramid_t77_a175_b2.0.p'
 results_file = '../results.csv'
 f = open(results_file, 'w')
 f.close()
@@ -23,45 +27,56 @@ quality_scores = {}
 coverage_scores = {}
 comprehension_scores = {}
 
+print "test"
 for summary in summaries:
-    summary_slash= summary.rfind('/') + 1
-    summary_dot = summary.rfind('.')
-    summary_name = int(summary[summary_slash:summary_dot])
-    #print '\t\t{}'.format(summary_name)
-    sentences = sentencesFromSegmentations(summary)
-    scus = buildSCUlist(pyramid)
-    Graph = SummaryGraph(sentences, scus)
-    independentSet = Graph.independentSet
-    candidates = buildSCUcandidateList(independentSet)
-    results = processResults(candidates, independentSet)
-    rearranged_results = scusBySentences(results)
-    score, matched_cus = getScore(rearranged_results, scus)
-    size_file = pyramid.replace('.p', '.size').replace('pyramids/', 'sizes/')
-    count_by_weight, avg = getLayerSizes(size_file)
-    raw_scores[summary_name] = score
-    quality = float(score)/maxRawScore(count_by_weight, matched_cus)
-    coverage = float(score)/maxRawScore(count_by_weight, avg)
-    comprehension = float((quality + coverage)) / 2
-    quality_scores[summary_name] = quality
-    coverage_scores[summary_name] = coverage
-    comprehension_scores[summary_name] = comprehension
+    if os.path.isdir(summary):
+        summ = glob.iglob(summary+'/*')
+        for fn in summ:
+            #print fn 
+            #if str(fn[:-5]) == '.segs':
+            if fn.endswith('.ls'):
+                print "current file, ", fn
+                summary_slash= fn.rfind('/') + 1
+                summary_dot = fn.rfind('.')
+                summary_name = fn[summary_slash:summary_dot]
+                sentences = sentencesFromSegmentations(fn)
+                scus = buildSCUlist(pyramid)
+                Graph = SummaryGraph(sentences, scus)
+                independentSet = Graph.independentSet
+                candidates = buildSCUcandidateList(independentSet)
+                results = processResults(candidates, independentSet)
+                rearranged_results = scusBySentences(results)
+                score, matched_cus = getScore(rearranged_results, scus)
+                size_file = pyramid.replace('.p', '.size').replace('pyrs/pyramids/', 'sizes/')
+                count_by_weight, avg = getLayerSizes(size_file)
+                raw_scores[summary_name] = score
+                quality = float(score)/maxRawScore(count_by_weight, matched_cus)
+                coverage = float(score)/maxRawScore(count_by_weight, avg)
+                comprehension = float((quality + coverage)) / 2
+                quality_scores[summary_name] = quality
+                coverage_scores[summary_name] = coverage
+                comprehension_scores[summary_name] = comprehension
+            else:
+                pass
 
-raw_scores = sorted(raw_scores.items(), key=lambda x: x[0])
-raw_scores = [s[1] for s in raw_scores]
-quality_scores = sorted(quality_scores.items(), key=lambda x: x[0])
-quality_scores = [s[1] for s in quality_scores]
-coverage_scores = sorted(coverage_scores.items(), key=lambda x: x[0])
-coverage_scores = [s[1] for s in coverage_scores]
-comprehension_scores = sorted(comprehension_scores.items(), key=lambda x: x[0])
-comprehension_scores = [s[1] for s in comprehension_scores]
-
-score_tables = ['raw', 'quality', 'coverage', 'comprehension']
-scores = [raw_scores, quality_scores, coverage_scores, comprehension_scores]
+#score_tables = ['raw', 'quality', 'coverage', 'comprehension']
+#scores = [raw_scores, quality_scores, coverage_scores, comprehension_scores]
 
 with open(results_file, 'a') as f:
     w = csv.writer(f)
     w.writerow(['Summary'] + score_tables)
     for n, summary in enumerate(summaries):
-    	w.writerow([filename(summary)] + [s[n] for s in scores])
-    	print '{} | {:>2} | {:.3f} | {:.3f} | {:.3f}'.format(filename(summary), scores[0][n], scores[1][n], scores[2][n], scores[3][n])
-        
+    	#w.writerow([filename(summary)] + [s[n] for s in scores])
+    	if os.path.isdir(summary):
+            summ = glob.iglob(summary+'/*')
+            for fn in summ:
+                #if fn[:-5] == '.segs':
+                 if fn.endswith('.ls'):
+                    summary_slash= fn.rfind('/') + 1
+                    summary_dot = fn.rfind('.')
+                    summary_name = fn[summary_slash:summary_dot]
+                    #print "Raw score for summary ", summary_name, ": ", raw_scores[summary_name]
+                    output = [summary_name, raw_scores[summary_name],quality_scores[summary_name],coverage_scores[summary_name],comprehension_scores[summary_name]]
+                    w.writerow(output)
+                    print '{} | {:>2} | {:.3f} | {:.3f} | {:.3f}'.format("summary name", "Raw score", "Quality score", "Coverage score", "Comprehension score")
+                    print '{} | {:>2} | {:.3f} | {:.3f} | {:.3f}'.format(summary_name, raw_scores[summary_name], quality_scores[summary_name],coverage_scores[summary_name],comprehension_scores[summary_name])

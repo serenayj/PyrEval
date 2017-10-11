7546b6ef97435b551c116fdd5447fd1527df3791
yug125@psu.edu <yug125@psu.edu>
2017-10-11 18:30:09 -0400

    Add license

--- split-sent.py
+++ split-sent.py
@@ -1,3 +1,21 @@
+# 	Script for cleaning the files and splitting the lines
+
+#    Copyright (C) 2017 Yanjun Gao
+
+#    This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from nltk import sent_tokenize,word_tokenize
 import re
 import os
--- stanford.py
+++ stanford.py
@@ -1,3 +1,20 @@
+# Script for running StanfordCoreNLP
+
+#    Copyright (C) 2017 Yanjun Gao
+
+#    This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Aug 6, 2017
 # Step 0, move this file to stanford-corenlp folder 
 # mode: 1: peer summries; 2: wise_crowd_summaries, 3:test_summaries 

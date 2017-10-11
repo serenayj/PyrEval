2b7019aa355692537f56abd810339868e5047a2b
yug125@psu.edu <yug125@psu.edu>
2017-10-11 18:31:04 -0400

    Add license

--- Preprocess/sentparser.py
+++ Preprocess/sentparser.py
@@ -4,6 +4,22 @@
 # SETENCE DECOMPOSITION PARSER 
 # This is a script for handling the output from Stanford CoreNLP constutiency parser, dependency parser 
 # The script is for sentence decomposition. For a given sentence, we extract a few segmentations which contain different segments  
+
+#Copyright (C) 2017  Yanjun Gao
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  
 # The code has four main modules: 
 # Helper functions: Accessories function for processing the tree
@@ -11,9 +27,7 @@
 # Sentence Segmentation: Rearrange the segment into a complete pieces 
 # Main Procedure: A complete pipeline  
 
-# TO-DO list:
-# csubj case
-# subject assignment(done) 
+
 
 
 from bs4 import BeautifulSoup

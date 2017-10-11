75c238abedf472103f23fcbf7ad62e406e51ab82
yug125@psu.edu <yug125@psu.edu>
2017-10-11 18:31:40 -0400

    Add license

--- Scoring/scoring.py
+++ Scoring/scoring.py
@@ -1,3 +1,21 @@
+#   Script for scoring summaries by the pyramid
+
+#    Copyright (C) 2017 Yanjun Gao
+
+#    This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from lib_scoring import sentencesFromSegmentations, SummaryGraph, buildSCUcandidateList, filename, formatVerboseOutput
 from lib_scoring import getScore, getLayerSizes, processResults, scusBySentences, maxRawScore, readPyramid
 from scipy.stats import pearsonr as pearson
